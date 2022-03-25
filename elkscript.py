0213955b84d57227e087e1769b85e05c4a811bff
Dorian Vandamme <dorian.vandamme@gologic.ca>
2022-03-25 16:44:10 -0400

    added a main function to the elkscript

--- elkscript.py
+++ elkscript.py
@@ -1,5 +1,7 @@
+import sys,getopt
 import requests
 import json
+import configparser
 
 def elk_create_new_index(index_name,config):
     """
@@ -13,3 +15,24 @@ def elk_create_new_index(index_name,config):
         mapping = json.load(file)
         response = requests.put(elk_url+"/"+index_name,headers=headers,data=json.dumps(mapping))
         return response
+
+def main(argv):
+    """
+    This script create a new index and post the jira logs in an elk instance
+    """
+    index_name = None
+    try:
+        opts, args = getopt.getopt(argv,"i:",["index="])
+    except getopt.GetoptError:
+        print("jirascript.py -i <indexname>")
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-i", "--index"):
+            index_name = arg
+    
+    config = configparser.ConfigParser()
+    config.read('config.cfg')
+    response = elk_create_new_index(index_name,config)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
