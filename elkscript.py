8e11bacbca54a204e0eb68fa966d483a5702057a
Dorian Vandamme <dorian.vandamme@gologic.ca>
2022-03-29 16:54:35 -0400

    added a function to send jira logs to an elk index

--- elkscript.py
+++ elkscript.py
@@ -1,3 +1,4 @@
+from cmath import log
 import sys,getopt
 import requests
 import json
@@ -11,9 +12,25 @@ def elk_create_new_index(index_name,config):
     headers = {
     "Content-Type": "application/json",
     }
-    with open("mapping.json",'r',encoding="UTF=8") as file:
+    with open("mapping.json",'r',encoding="UTF-8") as file:
         mapping = json.load(file)
         response = requests.put(elk_url+"/"+index_name,headers=headers,data=json.dumps(mapping))
+        return response
+
+def elk_post_logs_bulk(json_file_path,index,config):
+    """
+    Post in bulk the logs registered in the json_file, in the given index
+    """
+    elk_url = config["ELK"]["ELASTICSEARCH_URL"]
+    headers = {
+    "Content-Type": "application/json",
+    }
+    with open(json_file_path,'r',encoding="UTF-8") as file:
+        logs = json.load(file)
+        to_insert = {"index":{}}
+        for i in range(0,len(logs)):
+            logs.insert(2*i,to_insert)
+        response = requests.post(elk_url+"/"+index+"/_bulk", headers=headers,data=json.dumps(logs))
         return response
 
 def main(argv):
@@ -21,18 +38,23 @@ def main(argv):
     This script create a new index and post the jira logs in an elk instance
     """
     index_name = None
+    logfile = None
     try:
-        opts, args = getopt.getopt(argv,"i:",["index="])
+        opts, args = getopt.getopt(argv,"i:l:",["index=","logfile="])
     except getopt.GetoptError:
-        print("jirascript.py -i <indexname>")
+        print("jirascript.py -i <indexname> -l <logfile>")
         sys.exit(2)
     for opt, arg in opts:
         if opt in ("-i", "--index"):
             index_name = arg
-    
+        elif opt in ("-l", "--logfile"):
+            logfile = arg
     config = configparser.ConfigParser()
     config.read('config.cfg')
     response = elk_create_new_index(index_name,config)
+    print(response)
+    response = elk_post_logs_bulk(logfile,index_name,config)
+    return response
 
 if __name__ == "__main__":
     main(sys.argv[1:])
