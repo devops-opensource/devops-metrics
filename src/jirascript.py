9a44dd8e0401ed608ee2f2128f5768049b19eb26
Dorian Vandamme <dorian.vandamme@gologic.ca>
2022-05-05 14:47:09 -0400

    refactored jirascript

--- src/jirascript.py
+++ src/jirascript.py
@@ -7,7 +7,7 @@ import json
 
 class JiraExporter:
 
-    def __init__(self,config, jira_type, project_name, epic_key):
+    def __init__(self,config, jira_type, project_key, epic_key):
         if(jira_type == "server"):
             self.email = config["JIRA_SERVER"]["jira_user_email"]
             self.passwd = config["JIRA_SERVER"]["jira_user_password"]
@@ -20,90 +20,72 @@ class JiraExporter:
         "Accept": "application/json",
         "Content-Type": "application/json",
         }
-        self.project_name = project_name
+        self.project_key = project_key
         self.epic_key = epic_key
+        self.project_id =  -1
+        self.new_status_dict = dict()
+        self.log_list = list()
 
-    def run():
-        return
+    def run(self):
+        self.project_id = self.get_project_id()
+        self.new_status_dict = self.get_new_statuses()
+        changelogs = self.get_status_change_logs()
+        self.log_list = self.transform_changelogs(changelogs)
+        return self.log_list
 
-    def get_status_change_logs(self,jira_type,project_name,epic_key,config):
+    def get_status_change_logs(self):
         """
         Get ticket changelogs from a jira server
         """
-        project_list_response = requests.get(self.jira_adress+"/rest/api/2/project",auth=(self.email, self.passwd), headers=self.headers)
-        if(project_list_response.status_code != 200):
-            print(project_list_response.content)
-            return #TODO lancer une exception
-
-        project_id = get_project_id(project_name,project_list_response.json())
-
-        status_list_response = requests.get(jira_adress+"/rest/api/2/project/"+project_id+"/statuses",auth=(email, passwd), headers=headers)
-        if(status_list_response.status_code != 200):
-            print(status_list_response.content)
-            return #TODO lancer une exception
-
-        new_statuses = get_new_statuses(status_list_response.json())
         
-        if(epic_key == ""):
-            response = requests.get(jira_adress+"/rest/api/2/search?jql=project="+
-                project_name+"&fields=issuetype,status,created,project,parent"+
-                "&expand=changelog&maxResults=200",auth=(email, passwd), headers=headers)
+        if(self.epic_key == ""):
+            response = requests.get(self.jira_adress+"/rest/api/2/search?jql=project="+
+                self.project_key+"&fields=issuetype,status,created,project,parent"+
+                "&expand=changelog&maxResults=200",auth=(self.email, self.passwd), headers=self.headers)
         else:
-            response = requests.get(jira_adress+"/rest/api/2/search?jql=cf[11200]="+
-                epic_key+"|key="+epic_key+"&fields=issuetype,status,created,project,parent,customfield_11200"+
-                "&expand=changelog&maxResults=200",auth=(email, passwd), headers=headers)
+            response = requests.get(self.jira_adress+"/rest/api/2/search?jql=cf[11200]="+
+                self.epic_key+"|key="+self.epic_key+"&fields=issuetype,status,created,project,parent,customfield_11200"+
+                "&expand=changelog&maxResults=200",auth=(self.email, self.passwd), headers=self.headers)
 
         if(response.status_code != 200):
             print(response.status_code) 
             return #TODO lancer une exception
-            
+        json = response.json()
         max_results = json["maxResults"]
         nb_of_pages = math.ceil(json["total"]/max_results)
-        
-        changelogs= response.json
+        print("max results: " + str(max_results) + " json total: " + str(json["total"]))
+        changelogs= response.json()["issues"]
 
         for i in range(0, nb_of_pages):
             if(i>0):
                 print(str(i)+"/"+str(nb_of_pages))
-                if(epic_key == ""):
-                    response = requests.get(jira_adress+"/rest/api/2/search?jql=project="+
-                        project_name+"&fields=issuetype,status,created,project,parent"+
-                        "&expand=changelog&maxResults=200&startAt="+str(i*max_results),auth=(email, passwd), headers=headers)
+                if(self.epic_key == ""):
+                    response = requests.get(self.jira_adress+"/rest/api/2/search?jql=project="+
+                        self.project_key+"&fields=issuetype,status,created,project,parent"+
+                        "&expand=changelog&maxResults=200&startAt="+str(i*max_results),auth=(self.email, self.passwd), headers=self.headers)
                 else:
-                    response = requests.get(jira_adress+"/rest/api/2/search?jql=cf[11200]="+
-                        epic_key+"|key="+epic_key+"&fields=issuetype,status,created,project,parent,customfield_11200"+
-                        "&expand=changelog&maxResults=200&startAt="+str(i*max_results),auth=(email, passwd), headers=headers)
-                changelogs.append(response.json())
+                    response = requests.get(self.jira_adress+"/rest/api/2/search?jql=cf[11200]="+
+                        self.epic_key+"|key="+self.epic_key+"&fields=issuetype,status,created,project,parent,customfield_11200"+
+                        "&expand=changelog&maxResults=200&startAt="+str(i*max_results),auth=(self.email, self.passwd), headers=self.headers)
+                changelogs.extend(response.json()["issues"])
 
         return changelogs
 
-    def transform_changelogs(changelogs):
-        for i in range(0, nb_of_pages):
-            if(i>0):
-                print(str(i)+"/"+str(nb_of_pages))
-                if(epic_key == ""):
-                    response = requests.get(jira_adress+"/rest/api/2/search?jql=project="+
-                        project_name+"&fields=issuetype,status,created,project,parent"+
-                        "&expand=changelog&maxResults=200&startAt="+str(i*max_results),auth=(email, passwd), headers=headers)
-                else:
-                    response = requests.get(jira_adress+"/rest/api/2/search?jql=cf[11200]="+
-                        epic_key+"|key="+epic_key+"&fields=issuetype,status,created,project,parent,customfield_11200"+
-                        "&expand=changelog&maxResults=200&startAt="+str(i*max_results),auth=(email, passwd), headers=headers)
-                json = response.json()
-            issue_list = json["issues"]
-            for issue in issue_list:
-                new_status_tuple = new_statuses[issue["fields"]["issuetype"]["id"]]
-                log = create_log(issue, issue["fields"]["created"], new_status_tuple[0], new_status_tuple[1])
-                log_list.append(log)
-                for changelog in issue["changelog"]["histories"]:
-                    for item in changelog["items"]:
-                        if(item["field"]=="status"):
-                            log = create_log(issue,changelog["created"],item["to"],item["toString"])
-                            log_list.append(log)
+    def transform_changelogs(self,changelogs):
+        log_list = []
+        for issue in changelogs:
+            new_status_tuple = self.new_status_dict[issue["fields"]["issuetype"]["id"]]
+            log = self.create_log(issue, issue["fields"]["created"], new_status_tuple[0], new_status_tuple[1])
+            log_list.append(log)
+            for changelog in issue["changelog"]["histories"]:
+                for item in changelog["items"]:
+                    if(item["field"]=="status"):
+                        log = self.create_log(issue,changelog["created"],item["to"],item["toString"])
+                        log_list.append(log)
 
         return log_list
 
-    def create_log(issue, timestamp, status_id, status_name):
+    def create_log(self,issue, timestamp, status_id, status_name):
         """
         Create a log entry
         issue["fields"]["created"], issue["id"],issue["key"],issue["project"]["id"], issue["project"]["key"]
@@ -129,23 +111,33 @@ class JiraExporter:
         log["status_name"] = status_name
         return log
 
-    def get_project_id(project_key, project_list):
+    def get_project_id(self):
         """
-        return the id associated to a project key in a project list
+        return the id associated to a project key after getting the project list from the jira instance
         """
+        project_list_response = requests.get(self.jira_adress+"/rest/api/2/project",auth=(self.email, self.passwd), headers=self.headers)
+        if(project_list_response.status_code != 200):
+            print(project_list_response.content)
+            return #TODO lancer une exception
+        
+        project_list = project_list_response.json()
+
         for project in project_list:
-            if(project["key"] == project_key):
+            if(project["key"] == self.project_key):
                 return project["id"]
         return None
 
-    def get_new_statuses(config):
+    def get_new_statuses(self):
         """
         return a dict containing the id and name of the new status for each issue type
         """
-        status_list_response = requests.get(jira_adress+"/rest/api/2/project/"+project_id+"/statuses",auth=(email, passwd), headers=headers)
+        status_list_response = requests.get(self.jira_adress+"/rest/api/2/project/"+self.project_id+"/statuses",
+            auth=(self.email, self.passwd), headers=self.headers)
         if(status_list_response.status_code != 200):
             print(status_list_response.content)
             return #TODO lancer une exception
+
+        status_list = status_list_response.json()
 
         new_status_dict = dict() 
         for ticket in status_list:
@@ -154,7 +146,7 @@ class JiraExporter:
                     new_status_dict[ticket["id"]]=(status["id"],status["name"])
         return new_status_dict
 
-    def save_logs_in_csv(file_path, log_list):
+    def save_logs_in_csv(self,file_path):
         """
         Save a list of Jira logs as a csv file
         """
@@ -164,17 +156,17 @@ class JiraExporter:
         with open(file_path, 'w', encoding="UTF-8", newline='') as file:
             write = csv.writer(file)
             write.writerow(fields)
-            for log in log_list:
+            for log in self.log_list:
                 write.writerow(log.values())
 
-    def save_logs_in_json(file_path, log_list):
+    def save_logs_in_json(self,file_path):
         """
         Save a list of Jira logs as a json file
         """
         if(not file_path.endswith(".json")):
             file_path = file_path+".json"
         with open(file_path,'w',encoding="UTF=8") as file:
-            json.dump(log_list,file)
+            json.dump(self.log_list,file)
 
 
 def main(argv):
@@ -206,12 +198,13 @@ def main(argv):
         elif opt in ("-e", "--epickey"):
             epic_key = arg
     config = configparser.ConfigParser()
-    config.read('config.cfg')
-    results = get_status_change_logs(jira_type,project_name,epic_key,config)
+    config.read('../config.cfg')
+    jira_exp = JiraExporter(config, jira_type, project_name, epic_key)
+    results = jira_exp.run()
     if(output_type == "csv"):
-        save_logs_in_csv(output_file,results)
+        jira_exp.save_logs_in_csv(output_file)
     else:
-        save_logs_in_json(output_file,results)
+        jira_exp.save_logs_in_json(output_file)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
