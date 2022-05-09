0f11fe0349c5ea7b65f544598dafd907c4ea46a2
Sebastien Bernard <sebastien.bernard@desjardins.com>
2022-05-09 15:53:31 -0400

    working

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 from src import jirascript as jira_importer
 from src import splunkscript as splunk_importer
-import sys, configparser, argparse
+import configparser, argparse
 
 ########### Arguments ############
 parser = argparse.ArgumentParser(description="Permet d'envoyer un rapport d'exécution de tests ainsi que ses captures d'écran dans Xray")
 parser.add_argument("exporter", help="Clé du projet JIRA")
 parser.add_argument("importer", help="Clé du plan de test JIRA")
-
-
-
+parser.add_argument("-e","--epicKey", help="Clé du plan de test JIRA")
 parser.add_argument('-d', '--debug', action='store_true',  help="shows output")
 args = parser.parse_args()
 
@@ -16,5 +14,10 @@ if __name__ == "__main__":  # pragma: no cover
     config = configparser.ConfigParser()
     config.read('config.cfg')
 
-    logs = jira_importer.main(sys.argv[1:])
+    isJiraServer = "server" if args.exporter == "jira_server" else ""
+    epicKey = args.epicKey if args.epicKey else ""
+
+    jira_exporter = jira_importer.JiraExporter(config, isJiraServer ,epicKey)
+    logs = jira_exporter.run()
+
     splunk_importer.export_log(logs,config)
