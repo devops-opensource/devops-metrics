b3a965c0cacee6c65b629e3c334b674d96ec53a1
gologic-ben <benjamin.lallement@gologic.ca>
2022-10-18 22:15:55 -0400

    mysql loader: convert to datetime fields

--- loader/mysql/loader.py
+++ loader/mysql/loader.py
@@ -1,25 +1,27 @@
-from sqlalchemy import create_engine
+import sqlalchemy
 import pandas as pd
 from glob import glob
 
 # User, pw, and db are being imported from dbconfig.py file to mask credentials
-db_data = ("mysql://{user}:{pw}@{host}/{db}".format(host='localhost:3306', user='root', pw='rootpassword', db='metricsDB'))
+db_data = ("mysql://{user}:{pw}@{host}/{db}".format(host='localhost:3306', user='gologic', pw='gologic', db='metricsDB'))
 # Using 'create_engine' from sqlalchemy to make the db connection
-engine = create_engine(db_data).connect()
+engine = sqlalchemy.create_engine(db_data).connect()
 # set the variables for the filepaths of where the source files are and where to move them to
-releaseFile = '../examples/CART_versions.csv'
-statusFile = '../examples/CART_status_changes.csv'
+releaseFile = '../../examples/CART_versions.csv'
+statusFile = '../../examples/CART_status_changes.csv'
 
 # read file as CSV
 df = pd.read_csv(releaseFile)
 # print dataframe to the console
 print(df)
 # insert the dataframe into the db table
-df.to_sql(name='releases', con=engine, if_exists='replace', index=False)
+dtype = {"release_date": sqlalchemy.DateTime, "start_date": sqlalchemy.DateTime, "control_date": sqlalchemy.DateTime}
+df.to_sql(name='releases', con=engine, if_exists='replace', index=False, dtype=dtype)
 # print success message to the console
 print("release imported!")
 
 # status changes
 df = pd.read_csv(statusFile)
-df.to_sql(name='status_changes', con=engine, if_exists='replace', index=False)
+dtype = {"to_date": sqlalchemy.DateTime, "from_date": sqlalchemy.DateTime, "control_date": sqlalchemy.DateTime}
+df.to_sql(name='status_changes', con=engine, if_exists='replace', index=False, dtype=dtype)
 print("status changes imported!")
