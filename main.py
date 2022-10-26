067c5ddf21e4c1a21e8d3d0b1fbfa506af01916a
gologic-ben <benjamin.lallement@gologic.ca>
2022-10-25 22:24:50 -0400

    Add mysql loader in main

--- main.py
+++ main.py
@@ -1,36 +1,25 @@
-from src.common import common
 import configparser, argparse
 import jirascript as jira_exractor
+from mysql_loader import MySqlLoader
 
 parser = argparse.ArgumentParser()
 parser.add_argument("jira_token")
-parser.add_argument("influxdb_password")
+parser.add_argument("mysql_password")
 args = parser.parse_args()
 
 if __name__ == "__main__": 
     config = configparser.ConfigParser()
-    config.read("./config.cfg", encoding ="utf-8")
+    config.read("./config.gologic.cfg", encoding ="utf-8")
     jira_token = args.jira_token
-
-    exporter_parameters = {
-        "jira_token": jira_token,
-        "project_key": "GO-DEVOPS"
-    }
+    mysql_password = args.mysql_password
 
     jira_exporter = jira_exractor.JiraCloud(config, jira_token, "CART")
-    status_changes = jira_exporter.get_status_changes()
-    versions = jira_exporter.get_release_management()
-    jira_exractor.df_to_csv(status_changes, "examples/CART_status_changes.csv")
-    jira_exractor.df_to_csv(versions, "examples/CART_versions.csv")
-    # exporter = common.ExporterFactory("JiraCloud")
-    # exporter.initialize_data(config, exporter_parameters)
-    # exporter.extract_data()
+    status_changes_df = jira_exporter.get_status_changes()
+    versions_df = jira_exporter.get_release_management()
 
-    # importer_parameters = {
-    #     "password": args.influxdb_password,
-    # }
-    
-    # importer = common.ImporterFactory("InfluxDB")
-    # importer.initialize_data(config, importer_parameters)
-    # importer.adapt_data()
-    # importer.load_data()
+    jira_exractor.df_to_csv(status_changes_df, "examples/CART_status_changes.csv")
+    jira_exractor.df_to_csv(versions_df, "examples/CART_versions.csv")
+
+    mysql_loader = MySqlLoader(config, mysql_password)
+    mysql_loader.loadReleasesFromDf(versions_df)
+    mysql_loader.loadStatusChangesFromDf(status_changes_df)
