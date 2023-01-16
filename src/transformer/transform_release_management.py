946919aa525fc0dbcf592b44a51fdd27d3a92dd0
Dorian-Vandamme <dorian.vandamme@gologic.ca>
2023-01-16 16:19:23 -0500

    fix csv loader erros added a function to change a column to datetime type

--- src/transformer/transform_release_management.py
+++ src/transformer/transform_release_management.py
@@ -6,16 +6,18 @@ class TransformReleaseManagement:
         if(df_versions.empty):
             return pd.DataFrame()
         df_versions_released = df_versions[df_versions["released"] == True]
-        if ("release_date" in df_versions_released):
-            df_versions_released.loc[:,"release_date"] = pd.to_datetime(df_versions_released["release_date"], utc=True, errors="coerce").dt.tz_convert(None)
-        else:
-            df_versions_released["release_date"] = None
-        if ("start_date" in df_versions_released):
-            df_versions_released["start_date"] = pd.to_datetime(df_versions_released["start_date"], utc=True, errors="coerce").dt.tz_convert(None)
-        else:
-            df_versions_released["start_date"] = None
+
+        df_versions_released = self.convert_column_to_datetime("release_date", df_versions_released)
+        df_versions_released = self.convert_column_to_datetime("start_date", df_versions_released)
 
         df_versions_released["event_type"] = "release_management"
         df_versions_released["control_date"] = df_versions_released["release_date"]
 
         return df_versions_released
+
+    def convert_column_to_datetime(self, column, df):
+        if (column in df):
+            df[column] = pd.to_datetime(df[column], utc=True, errors="coerce").dt.tz_convert(None)
+        else:
+            df[column] = None
+        return df
