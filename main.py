1378eeaabd05408db7fee0cfb308944e484733cf
Dorian-Vandamme <101835162+Dorian-Vandamme@users.noreply.github.com>
2023-03-22 10:58:14 -0400

    Refactor/jira extractor transformer (#42)
    * refactored the extraction and transformation function
    Api calls have been seperated from the logic
    still WIP
    
    * removed jiracloud_extractor since it has been refactored
    
    * nettoyage du config local
    
    * fix csv loader erros
    added a function to change a column to datetime type
    
    * code quality and small refactoring
    
    * code quality
    
    * code quality
    
    * pep8 formatting
    
    * reformattage

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 import configparser
 import argparse
-from src.extractor.jiracloud_extractor import JiraCloud
+from src.extractor.jiracloud_exporter import JiracloudExporter
 from src.loader.csv_loader import CsvLoader
 from src.loader.mysql_loader import MySqlLoader
 from src.common import common
+from src.transformer.transform_release_management import (
+    TransformReleaseManagement,
+)
+from src.transformer.transform_status_change import TransformStatusChanges
 
 parser = argparse.ArgumentParser()
 parser.add_argument("config_file")
+parser.add_argument("Exporter", type=str, help="Loader type: JiraCloud")
 parser.add_argument("Loader", type=str, help="Loader type: CSV or MYSQL")
 args = parser.parse_args()
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     config = configparser.ConfigParser()
     config.read(args.config_file, encoding="utf-8")
 
+    exporter_name = args.Exporter
+    exporter = common.ExporterFactory(exporter_name)
+
     loader_name = args.Loader
     loader = common.LoaderFactory(loader_name)
-
     try:
-        jira_exporter = JiraCloud(config)
+        exporter.initialize_data(config)
     except Exception as err:
         print(err)
         quit("Unable to connect to JIRA project")
@@ -31,9 +38,23 @@ if __name__ == "__main__":
 
     # Extract and Transform status changes and release
     print("Extract JIRA data then Transform as status_changes and releases")
-    df_dict = {}
-    df_dict["status_changes"] = jira_exporter.get_status_changes()
-    df_dict["releases"] = jira_exporter.get_release_management()
+    raw_data = exporter.extract_data()
+    adapted_data = exporter.adapt_data(raw_data)
+    release_transformer = TransformReleaseManagement()
+    df_release_management = release_transformer.transform_release_management(
+        adapted_data["versions"]
+    )
+
+    status_changes_transformer = TransformStatusChanges(
+        config, df_release_management
+    )
+    df_status_cnhages = status_changes_transformer.transform_status_changes(
+        adapted_data["status_changes"]
+    )
+    df_dict = {
+        "status_changes": df_status_cnhages,
+        "releases": df_release_management,
+    }
 
     loader.load_data(df_dict)
 
--- src/common/common.py
+++ src/common/common.py
@@ -1,12 +1,11 @@
-from src.extractor import exporter, jira_exporter
-from src.loader import mysql_loader, csv_loader,loader
+from src.extractor import exporter, jiracloud_exporter
+from src.loader import mysql_loader, csv_loader, loader
+import pandas as pd
 
 
 def ExporterFactory(type) -> exporter.Exporter:
     """Factory Method"""
-    localizers = {
-        "JiraCloud": jira_exporter.JiraExporter
-    }
+    localizers = {"JiraCloud": jiracloud_exporter.JiracloudExporter}
     return localizers[type]()
 
 
@@ -14,6 +13,16 @@ def LoaderFactory(type) -> loader.Loader:
     """Factory Method"""
     localizers = {
         "MYSQL": mysql_loader.MySqlLoader,
-        "CSV": csv_loader.CsvLoader
+        "CSV": csv_loader.CsvLoader,
     }
     return localizers[type]()
+
+
+def convert_column_to_datetime(column, df):
+    if column in df:
+        df[column] = pd.to_datetime(
+            df[column], utc=True, errors="coerce"
+        ).dt.tz_convert(None)
+    else:
+        df[column] = None
+    return df
