175bf3b37c26b6e488567ecc1f13bcab63c3d865
seb-devops <80295674+seb-devops@users.noreply.github.com>
2023-03-24 15:48:37 -0400

    Update common.py

--- src/common/common.py
+++ src/common/common.py
@@ -1,6 +1,22 @@
 from src.extractor import exporter, jiracloud_exporter
-from src.loader import mysql_loader, csv_loader, loader
+from src.loader import mysql_loader, csv_loader, splunk_loader, loader
 import pandas as pd
+from functools import wraps
+import time
+
+
+def execution_time(func):
+    @wraps(func)
+    def timeit_wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        total_time = end_time - start_time
+        print(f'Function {func.__name__}{args} {kwargs} '
+              + 'Took {total_time:.4f} seconds')
+        return result
+    return timeit_wrapper
+
 
 
 def ExporterFactory(type) -> exporter.Exporter:
@@ -14,9 +30,9 @@ def LoaderFactory(type) -> loader.Loader:
     localizers = {
         "MYSQL": mysql_loader.MySqlLoader,
         "CSV": csv_loader.CsvLoader,
+        "SPLUNK": splunk_loader.SplunkLoader
     }
     return localizers[type]()
-
 
 def convert_column_to_datetime(column, df):
     if column in df:
